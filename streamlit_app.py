338357d0137854084113fe158c04f197885f6520
S Papun Kumar Das <88581885+PapunKumarDas@users.noreply.github.com>
2023-02-23 14:46:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('hey this is papun kumar')
-streamlit.header(' breakfast menu')
-streamlit.text('puri upma')
-streamlit.text('bara piaji')
-streamlit.text('idili dosa')
+streamlit.title('hey guluu')
+streamlit.header('Your name:')
+streamlit.text('tinguuu')
+streamlit.text('pinguuuu')
+streamlit.text('kukuu baby')

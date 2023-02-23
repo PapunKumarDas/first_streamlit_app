70fb8a092ea267c5983679d97d24f1d27b9633a1
S Papun Kumar Das <88581885+PapunKumarDas@users.noreply.github.com>
2023-02-23 14:44:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('hey this is papun kumar')
+streamlit.header(' breakfast menu')
+streamlit.text('puri upma')
+streamlit.text('bara piaji')
+streamlit.text('idili dosa')
